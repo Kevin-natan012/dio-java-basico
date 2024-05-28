ef7d9a816a2335402d4e1f8ef7349ba41b247c6f
Kevin <kevinnatan97@gmail.com>
2024-05-27 22:10:54 -0300

    controle de fluxo

--- DesafioControleFluxo/src/App.java
+++ DesafioControleFluxo/src/App.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("Digite 2 numeros inteiros");
         Scanner terminal = new Scanner(System.in);
         int parametroUm = terminal.nextInt();
         int parametroDois = terminal.nextInt();
 
+
+        
+
     }
 
-    static void contar(int parametroUm, int parametroDois){
-        int contagem = parametroUm - parametroDois;
-        for(int i = 0; contagem <=i; i++){
-            System.out.println(contagem);
+    static void contar(int parametroUm, int parametroDois)throws ParametrosInvalidosException{
+
+        int contagem = parametroDois - parametroUm;
+        for(int i = 0; i <= contagem; i++){
+            System.out.println("a sequencia é: "+i);
         }
     }
 
